22ec3ec3f37a62c4a5e9ceb83a0b67b0538dc42b
gayathri.er <gayathri.er@zohocorp.com>
2023-04-16 12:16:01 +0530

    Add python equivalent module for seq2seq

--- extras/spacy/common_features.py
+++ extras/spacy/common_features.py
@@ -6,6 +6,8 @@ en_tokenizer = spacy.load("en_core_web_sm")
 input_text = "this is a sample sentence! let's see"
 tokenized_text: List[str] = [
     token.text.lower() for token in en_tokenizer.tokenizer(input_text)
-]
+]   # token -> 'spacy.tokens.token.Token'
 
 print(f"Input text: {input_text}\nTokenized text: {tokenized_text}\n")
+# Input text: this is a sample sentence! let's see
+# Tokenized text: ['this', 'is', 'a', 'sample', 'sentence', '!', 'let', "'s", 'see']
